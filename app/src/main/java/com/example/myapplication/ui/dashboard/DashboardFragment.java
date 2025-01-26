aae27e8c0489028521e31dff63502041a4c76771
raphael1-w <himwong.raphael@gmail.com>
2025-01-26 20:28:27 +0000

    Updated greeting text on capture page

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.TokenWatcher;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,13 +27,16 @@ import com.example.myapplication.databinding.FragmentDashboardBinding;
 import com.example.myapplication.ui.recording.RecordingFragment;
 import com.google.android.material.button.MaterialButton;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
+    private MediaRecorder mediaRecorder;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
 
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
@@ -39,32 +44,46 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final TextView textView = binding.promptText;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         MaterialButton recordButton = binding.getRoot().findViewById(R.id.record_button);
         recordButton.setOnClickListener(v -> {
             if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, 200);
             } else {
                 Toast.makeText(requireContext(), "Starting", Toast.LENGTH_SHORT).show();
-                startRecordingFragment();
+                startRecording();
             }
         });
         return root;
     }
 
-    private void startRecordingFragment() {
-        // Start recording fragment
+    private void startRecording() {
+        String audioFilePath = Objects.requireNonNull(requireContext().getExternalFilesDir(null)).getAbsolutePath() + "/recording.3gp";
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(audioFilePath);
         try {
-            NavController navcontroller = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
-            navcontroller.navigate(R.id.navigation_recording);
-        } catch (Exception e) {
-            Log.e("FragmentError", "Error starting recording fragment: " + e.getMessage());
-            // Handle the error appropriately
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            Toast.makeText(getContext(), "Recording started", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Recording failed", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        if (mediaRecorder != null) {
+            Toast.makeText(getContext(), "Recording stopped", Toast.LENGTH_SHORT).show();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
         binding = null;
     }
 }
--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.java
@@ -4,13 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Calendar;
+
 public class DashboardViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+    // Get current time to display appropriate greeting
+    // TODO: implement translation
+    public String getGreeting() {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if ((hour >= 0) && (hour < 12)) {
+            return "Good Morning";
+        } else if (hour >= 12 && hour < 16) {
+            return "Good Afternoon";
+        } else {
+            return "Good Evening";
+        }
+    }
+
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("What's on your mind?");
+        mText.setValue(getGreeting());
     }
 
     public LiveData<String> getText() {
