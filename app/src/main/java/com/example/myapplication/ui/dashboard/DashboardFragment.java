05e4e12d65ef15fc82d22c918123d4b3c1076b80
raphael1-w <himwong.raphael@gmail.com>
2025-01-27 02:45:40 +0000

    Added functionality to send or record message based on text in capture bar

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -2,29 +2,26 @@ package com.example.myapplication.ui.dashboard;
 
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.TokenWatcher;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
-import com.example.myapplication.ui.recording.RecordingFragment;
 import com.google.android.material.button.MaterialButton;
 
 import java.io.IOException;
@@ -33,6 +30,7 @@ import java.util.Objects;
 public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
     private MediaRecorder mediaRecorder;
+    boolean commitButtonIsRecord = true;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -47,50 +45,86 @@ public class DashboardFragment extends Fragment {
         final TextView textView = binding.promptText;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        MaterialButton recordButton = binding.getRoot().findViewById(R.id.record_button);
-        recordButton.setOnClickListener(v -> {
-            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, 200);
-            } else {
-                if (mediaRecorder != null) {
-                    stopRecording();
+        // Get the commit button
+        MaterialButton commitButton = binding.getRoot().findViewById(R.id.commit_button);
+        // React to the commit button being clicked
+        commitButton.setOnClickListener(v -> {
+            if (commitButtonIsRecord) {
+                // If the commit button is set to record, start recording
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, 200);
                 } else {
-                    startRecording();
+                    record();
+                }
+            } else {
+                // If the commit button is set to send, send the message
+                send();
+            };
+        });
+
+        // Get the input field
+        EditText editText = binding.getRoot().findViewById(R.id.inputField);
+        // Listen for changes to the input field
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Change the icon of the commit button based if the input field has text
+                if (s.length() > 0) {
+                    commitButtonIsRecord = false;
+                    commitButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_send, null));
+                } else {
+                    commitButtonIsRecord = true;
+                    commitButton.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_mic, null));
                 }
             }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {}
         });
         return root;
     }
 
-    private void startRecording() {
-        String audioFilePath = Objects.requireNonNull(requireContext().getExternalFilesDir(null)).getAbsolutePath() + "/recording.3gp";
-        mediaRecorder = new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.setOutputFile(audioFilePath);
-        try {
-            mediaRecorder.prepare();
-            mediaRecorder.start();
-            Toast.makeText(getContext(), "Recording started", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Toast.makeText(getContext(), "Recording failed", Toast.LENGTH_SHORT).show();
+    private void record() {
+        if (mediaRecorder == null) {
+            String audioFilePath = Objects.requireNonNull(requireContext().getExternalFilesDir(null)).getAbsolutePath() + "/recording.3gp";
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(audioFilePath);
+            try {
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+                Toast.makeText(getContext(), "Recording started", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(getContext(), "Recording failed", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "Recording stopped", Toast.LENGTH_SHORT).show();
+            mediaRecorder.release();
+            mediaRecorder = null;
         }
     }
 
-    private void stopRecording() {
-        Toast.makeText(getContext(), "Recording stopped", Toast.LENGTH_SHORT).show();
-        mediaRecorder.release();
-        mediaRecorder = null;
 
+    private void send() {
+        //TODO: Implement sending the message
+        Toast.makeText(getContext(), "Message sent", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        // Release the media recorder if it is currently active
         if (mediaRecorder != null) {
-            stopRecording();
+            mediaRecorder.release();
+            mediaRecorder = null;
         }
+
         binding = null;
     }
 }
